4e997ef17147bd57bbbd43dd5645b5af45ccf5ff
Micah Cowan <micah@addictivecode.org>
2020-07-21 05:37:38 -0700

    Rework so that output flips only on letters

--- mixcase.s
+++ mixcase.s
@@ -2,30 +2,40 @@
 
 Scr_A   = $C1
 Scr_Z   = $DA
-LCBit   = $20
+LcBit   = $20
+IsLcBit = $01
+IsUcBit = $02
 MON_COut= $FDF0
 
         .org $300
         CLD
-        PHA
-        LDA Flag
-        EOR #$FF
-        CMP #$01
+        STX Temp        ; Save existing value in X
+        TAX             ; Move character arg to X
+TstUC:  CPX #Scr_A      ; Note if the character in Acc is <'A' or >'Z'
+        BMI TstLC
+        CPX #(Scr_Z+1)
+        BCS TstLC
+        LDA #IsUcBit    ; Yes: note uppercase letter
+        BNE CheckFlipC  ; Skip lowercase check
+TstLC:  CPX #(Scr_A|LcBit); Note if the character in Acc is <'a' or >'z'
+        BMI COut        ; Not a letter - don't change flag nor char
+        CPX #(Scr_Z|LcBit+1)
+        BCS COut        ; Not a letter - don't change flag nor char
+        LDA #IsLcBit    ; Yes: note lowercase letter
+CheckFlipC:
+        CMP Flag        ; If letter is already the right one
+        BEQ DontFlipChar; then don't flip the char
+        TXA             ; Otherwise (<-- obliterating lc/uc info)
+        EOR #LcBit      ; flip the char
+        TAX
+DontFlipChar:
+        LDA #$03        ; Either way, flip the flag for next time
+                        ; because we found a letter
+        EOR Flag
         STA Flag
-        PLA
-        BCC Upper
-Lower:  
-        CMP #Scr_A      ; If the character in Acc is <'A' or >'Z', pass as-is
-        BMI COut
-        CMP #(Scr_Z+1)
-        BCS COut
-Flip:   EOR #LCBit      ; Flip lower-case/upper-case
-COut:   JMP MON_COut    ; INVOKE the monitor's COut1 function.
-Upper:  CMP #(Scr_A|LCBit); If the character in Acc is <'a' or >'z', pass as-is
-        BMI COut
-        CMP #(Scr_Z|LCBit+1)
-        BCS COut
-        BCC Flip
+COut:   TXA             ; Restore char arg to A
+        LDX Temp        ; Restore X orig value
+        JMP MON_COut    ; INVOKE the monitor's COut1 function.
         NOP
-Flag:   .BYTE $00
-Store_A:.BYTE $00
+Flag:   .BYTE IsLcBit   ; what kind of letter to output next time
+Temp:   .BYTE $00
